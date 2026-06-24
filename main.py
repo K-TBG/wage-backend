e321e1176d85b671b50e95acca9588034991719d
Kian <kian@thebullgroup.com>
2026-06-24 12:35:45 +0100

    Update main.py with synced version

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
-app = FastAPI()
+# Load the variables stored in .env
+load_dotenv
+
+app=FastAPI()
+
+#Read the API keys stored in .env
+SQUARE_KEY = os.getenv("SQUARE_API_KEY")
+DEPUTY_KEY = os.getenv("DEPUTY_API_KEY")
 
 @app.get("/")
 def home():
-    return {"message": "Backend is working!"}
+    return {
+        "message": "Backend is working!",
+        "square_key_loaded": SQUARE_KEY is not None,
+        "deputy_key_loaded": DEPUTY_KEY is not None
+    }
+
+@app.get("/wage-spend")
+def wage_spend(date:str):
+    return{
+        "requested_date":date,
+        "status": "Endpoint working"
+    }
